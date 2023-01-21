c5ab0f84198a0cbee35a96e3205236c2626e3803
mduckdev <mduckdev@gmail.com>
2023-01-21 18:37:07 +0100

    Working i think

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import Panel from './components/Panel.component';
 
@@ -6,6 +5,28 @@ function App() {
   return (
     <div className="App">
       <Panel></Panel>
+      <div className='list-wrapper'>
+        <p>Przykładowe operatory:</p>
+        <ul>
+          <li>E (stała e)</li>
+          <li>PI (stała pi)</li>
+          <li>x!</li>
+          <li>exp x (e^x)</li>
+          <li>sqrt x (pierwiastek kwadratowy z x)</li>
+          <li>cbrt x (pierwiastek sześcienny z x)</li>
+          <li>cos x</li>
+          <li>acos x</li>
+          <li>sin x</li>
+          <li>asin x</li>
+
+          <li>tan x</li>
+          <li>atan x</li>
+          <li>ln x</li>
+          <li>log10 x</li>
+          <li>log2 x</li>
+
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/Panel.component.tsx
+++ src/components/Panel.component.tsx
@@ -13,54 +13,112 @@ const Panel = () => {
     const [lowerBound, setLowerBound] = useState("");
     const [Equation, setEquation] = useState("");
     const [latexEquation, setLatexEquation] = useState("\\int_{}^{}xdx");
-    const [step, setStep] = useState<number | null>(null);
-    const handleInput = (e: React.ChangeEvent<HTMLInputElement>, source: SourceEnum) => {
-        if (source === SourceEnum.Equation) setEquation(e.target.value);
-        if (source === SourceEnum.lowerBound) setLowerBound(e.target.value);
-        if (source === SourceEnum.upperBound) setUpperBound(e.target.value);
-        if (source === SourceEnum.step) setStep(Number(e.target.value));
-
+    const [step, setStep] = useState<string | null>("");
+    const parseItem = (item: string) => {
+        item = item.replaceAll("*", "\\cdot ");
+        item = item.replaceAll("/", "\\div ");
+        item = item.replaceAll(",", ".");
+        item = item.replaceAll("E", "e ");
+        item = item.replaceAll("PI", "\\Pi ");
+        item = item.replaceAll("exp ", "e^");
+        item = item.replaceAll("sqrt ", "\\sqrt");
+        return item;
     }
-
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>, source: SourceEnum) => {
+        let isParsed = true;
+        try {
+            Parser.evaluate(e.target.value);
+        } catch (e) {
+            isParsed = false;
+        }
+        switch (source) {
+            case SourceEnum.Equation: {
+                setEquation(e.target.value);
+                break;
+            }
+            case SourceEnum.lowerBound: {
+                if ((isNaN(e.target.valueAsNumber) && !isParsed) || (Number(e.target.value) >= Number(upperBound) && upperBound !== "")) {
+                    e.target.classList.add("error");
+                    break;
+                }
+                setLowerBound(e.target.value);
+                e.target.classList.remove("error");
+                break;
+            }
+            case SourceEnum.upperBound: {
+                if ((isNaN(e.target.valueAsNumber) && !isParsed) || (Number(e.target.value) <= Number(lowerBound) && lowerBound !== "")) {
+                    e.target.classList.add("error");
+                    break;
+                }
+                setUpperBound(e.target.value);
+                e.target.classList.remove("error");
+                break;
+            }
+            case SourceEnum.step: {
+                if ((isNaN(e.target.valueAsNumber) && !isParsed) || e.target.valueAsNumber <= 0) {
+                    e.target.classList.add("error");
+                    break;
+                }
+                setStep(e.target.value);
+                e.target.classList.remove("error");
+                break;
+            }
+        }
+    }
     useEffect(() => {
-
-
-        setLatexEquation(`\\int_{${lowerBound}}^{${upperBound}}${Equation} dx`)
+        setLatexEquation(`\\int_{${parseItem(lowerBound)}}^{${parseItem(upperBound)}}${parseItem(Equation)} dx`);
     }, [upperBound, lowerBound, Equation]);
 
+
     const calculate = () => {
+        if (document.querySelector(".error")) { return alert("Nieprawidłowe dane") }
         if (upperBound !== "" && lowerBound !== "" && Equation !== "" && step !== null) {
             try {
                 const expression = parser.parse(Equation);
                 console.log(expression.evaluate({ x: lowerBound }));
-
-                let increase = (Number(upperBound) - Number(lowerBound)) / step
-                let iteration = Number(lowerBound) + increase;
+                let parsedUpperBound = parser.evaluate(upperBound);
+                let parsedLowerBound = parser.evaluate(lowerBound);
+                let increase = (parsedUpperBound - parsedLowerBound) / Number(step)
+                let iterator = parsedLowerBound + increase;
                 let sum = 0;
-                sum += Number(expression.evaluate({ x: lowerBound }));
-                for (iteration; iteration < Number(upperBound); iteration += increase) {
-                    sum += 2 * Number(expression.evaluate({ x: iteration }))
+                sum += Number(expression.evaluate({ x: parsedLowerBound }));
+                for (iterator; iterator < Number(parsedUpperBound); iterator += increase) {
+                    sum += 2 * Number(expression.evaluate({ x: iterator }))
                 }
-                sum += Number(expression.evaluate({ x: iteration }));
+                sum += Number(expression.evaluate({ x: iterator }));
                 sum *= increase / 2;
-
                 console.log(sum);
+                setLatexEquation(`\\int_{${parseItem(lowerBound)}}^{${parseItem(upperBound)}}${parseItem(Equation)} dx \\approx ${sum}`)
             } catch (e) {
-                console.log(e);
+                alert("Nie udało się obliczyć")
             }
+        } else {
+            return alert("Brak danych")
         }
     }
 
     return (
         <div className="appContainer">
             <div className="equation">
-                {latexEquation}
+                <img alt={latexEquation} src={`https://latex.codecogs.com/svg.image?${latexEquation}`} ></img>
             </div>
             <div className="inputs">
-                <input type="text" placeholder="Granica górna" onChange={e => handleInput(e, SourceEnum.upperBound)} ></input>
-                <input type="text" placeholder="Granica dolna" onChange={e => handleInput(e, SourceEnum.lowerBound)}></input>
-                <input type="number" placeholder="Krok" onChange={e => handleInput(e, SourceEnum.step)}></input>
-                <input type="text" placeholder="Wzór" onChange={e => handleInput(e, SourceEnum.Equation)} value={Equation}></input>
+                <div className="input-container">
+                    <input type="text" required={true} onChange={e => handleInput(e, SourceEnum.upperBound)} ></input>
+                    <label>Granica górna</label>
+                </div>
+                <div className="input-container">
+                    <input type="text" required={true} onChange={e => handleInput(e, SourceEnum.lowerBound)}></input>
+                    <label>Granica dolna</label>
+                </div>
+                <div className="input-container">
+                    <input type="text" required={true} onChange={e => handleInput(e, SourceEnum.step)}></input>
+                    <label>Krok</label>
+                </div>
+                <div className="input-container">
+                    <input type="text" required={true} onChange={e => handleInput(e, SourceEnum.Equation)} value={Equation}></input>
+                    <label>Wzór</label>
+                </div>
                 <button onClick={calculate} className="calculateButton">Oblicz</button>
             </div>
         </div>
